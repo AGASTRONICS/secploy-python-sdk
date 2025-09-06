79959a1ab7c1162729cb1bae581177edb6b98949
Abdulsamad Abdulganiyu <agastronics@gmail.com>
2025-09-06 12:13:07 +0100

    Modified: SDK v0.2.4

--- secploy/__init__.py
+++ secploy/__init__.py
@@ -1,7 +1,7 @@
-from secploy.client import SecployClient
-from secploy.schemas import SecployConfig
-from secploy.enums import LogLevel
+from .client import SecployClient
+from .schemas import SecployConfig
+from .enums import LogLevel
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 __all__ = ["SecployClient", "SecployConfig", "LogLevel"]
 
